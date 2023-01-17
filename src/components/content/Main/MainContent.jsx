3feff5e3e358f9266ba4a1dba0ebb66b6ed04aed
Sorokintech <sorokintech@gmail.com>
2023-01-17 14:52:50 +0300

    adding search by input and auth first steps

--- src/components/content/Main/MainContent.jsx
+++ src/components/content/Main/MainContent.jsx
@@ -32,26 +32,6 @@ export const MainContent = () => {
               </S.Card>
             </S.CardsItem>
           ))}
-          {/* {Array.from(new Array(20)).map((_, key) => (
-            <S.CardsItem key={key}>
-                              <S.Card>
-                                <S.CardImage>
-                                <NavLink to={'/article'}>
-                                    <img src="" alt=""></img>
-                                    </NavLink>
-                                </S.CardImage>
-                                <div>
-                                    <S.CardTitle>
-                                    Ракетка для большого тенниса Triumph Pro ST
-                                    </S.CardTitle>
-                                  <S.CardPrice>2&nbsp;200&nbsp;₽</S.CardPrice>
-                                  <S.CardPlace>Санкт Петербург</S.CardPlace>
-                                  <S.CardDate>Сегодня в&nbsp;10:45</S.CardDate>
-                                </div>
-                              </S.Card>
-                            </S.CardsItem>
-
-          ))} */}
         </S.Cards>
       </S.MainContent>
     </S.Container>
